70591d301045d4578a64660cdf875450bc826a96
Kingsley <dondecency11@gmail.com>
2023-02-15 06:34:08 +0000

    how

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -30,11 +30,11 @@ class TestFileStorageDocs(unittest.TestCase):
         """Set up for the doc tests"""
         cls.fs_f = inspect.getmembers(FileStorage, inspect.isfunction)
 
-    """def test_pep8_conformance_file_storage(self):
+    def test_pep8_conformance_file_storage(self):
         "Test that models/engine/file_storage.py conforms to PEP8."
         pep8s = pep8.StyleGuide(quiet=True)
         result = pep8s.check_files(['models/engine/file_storage.py'])
-        self.assertEqual(result.total_errors, 0)"""
+        self.assertEqual(result.total_errors, 0)
 
     def test_pep8_conformance_test_file_storage(self):
         """Test tests/test_models/test_file_storage.py conforms to PEP8."""
